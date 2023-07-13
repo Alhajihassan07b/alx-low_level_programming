c881595ea11b8eedc8b18db9891bbc354729cc20
Hassan Adamu Ibrahim <alhajihassan07b@gmail.com>
2023-07-13 08:38:56 -0700

    Write a C program that prints exactly

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
+ * main -it print out output using put
  *
- * main -it print out output using puts
- *
- * return: always (0)
+ * Return: always (0)
  *
  */
 int main(void)
