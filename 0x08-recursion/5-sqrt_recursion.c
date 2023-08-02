194875caa7c4a8630db2de98f1603b08858091dd
Hassan Adamu Ibrahim <alhajihassan07b@gmail.com>
2023-08-02 06:19:44 -0700

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * sqrt_a - return natural number
+ * @a: input
+ * @b: input
+ * @c: iteration
+ * Return: square root
+ *
+ */
+int sqrt_a(int a, int b)
+{
+	if (b * b == a)
+	{
+		return (b);
+	}
+	else if (b * b > a)
+	{
+		return (-1);
+	}
+	return (sqrt_a(a, b + 1));
+}
+
+#include "main.h"
+/**
+ * _sqrt_recursion - Entry point
+ * @n: input
+ * Return: (0)
+ *
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt_a(n, 0));
+}
