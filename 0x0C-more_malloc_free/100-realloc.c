f314f9f07e12975965635b4786071f0ec9582d90
Hassan Adamu Ibrahim <alhajihassan07b@gmail.com>
2023-08-10 06:02:53 -0700

    function that creates an array of integers

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _realloc -  function that reallocates a memory block using malloc and free
+ * @ptr: a pointer
+ * @old_size: input
+ * @new_size: input
+ * Return: pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
