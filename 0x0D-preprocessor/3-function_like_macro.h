8f55c97dec36eb37298ade985a4bf4b6173ffd7d
Hassan Adamu Ibrahim <alhajihassan07b@gmail.com>
2023-08-14 04:55:53 -0700

    compute absolute value

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef MACROLIKE_H
+#define MACROLIKE_H
+/**
+ * file: 3-function_like_macro
+ * @x: input value
+ * Desc: compute absolute value
+ */
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+#endif
