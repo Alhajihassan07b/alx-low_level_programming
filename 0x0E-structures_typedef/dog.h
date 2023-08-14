220650d6df4e25d9ac2048d5f4c68cc31f85db0f
Hassan Adamu Ibrahim <alhajihassan07b@gmail.com>
2023-08-14 06:09:44 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - define new type
+ * @name: first input
+ * @age: second input
+ * @owner: third input
+ * Desc: struct dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
