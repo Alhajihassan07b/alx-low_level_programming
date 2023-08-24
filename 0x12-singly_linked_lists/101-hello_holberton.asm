3d702d5d0c0f53873aed381fd79c44f896979cb2
Hassan Adamu Ibrahim <alhajihassan07b@gmail.com>
2023-08-24 06:19:41 -0700

    assembly that print Hello Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,11 @@
-global main
-extern printf
+global	main
+	extern	printf
 
 	format db 'Hello, Holberton\n',0
 
 main:
-	mov edi, format
-	xor eax, eax
-	call printf
-	mov eax, 0
+	mov  edi, format
+	xor  eax, eax
+	call  printf
+	mov  eax, 0
 	ret
