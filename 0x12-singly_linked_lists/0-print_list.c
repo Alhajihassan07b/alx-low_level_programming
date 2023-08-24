b08a6bcb40e90c7a1ff5dbb7cdae9e8e1dc73b36
Hassan Adamu Ibrahim <alhajihassan07b@gmail.com>
2023-08-24 04:05:11 -0700

    print all the element

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_list - function that print all the element
+ * @h: start pointer to linked
+ * Return: total node
+ */
+size_t print_list(const list_t *h)
+{
+	int x;
+
+	if (h == NULL)
+		return (0);
+	for (x = 1; h->next != NULL; x++)
+	{
+		if (h->str == NULL)
+			printf("[%u] %s\n", h->len, "(nil)");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	printf("[%u] %s\n", h->len, h->str);
+	return (x);
+}
