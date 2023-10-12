f5e9b0782f11b644b93d5dfb591ffb2979aab13f
Hassan Adamu Ibrahim <alhajihassan07b@gmail.com>
2023-10-12 09:55:57 -0700

    password

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - function that delete node
+ * @head: pointer to node
+ * @index: parameter
+ * Return: 1 if success -1 if fail
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+
+}
